c560d7252b25a4167420035fbceb798339d95080
Phantom <144142306+PhantomBoss05@users.noreply.github.com>
2025-11-18 14:09:36 +0100

    adding Day19 (not finished)

--- Puzzles/Day19/task02.py
+++ Puzzles/Day19/task02.py
@@ -1,30 +1,32 @@
 from Inputs.Reader import Reader
-from threading import Thread
 from re import finditer
-from tqdm import tqdm
-import itertools
+from queue import Queue
+from threading import Thread, Event, Lock
 
 name01: str = "input19.1.txt"
 name02: str = "input19.2.txt"
 replacement = Reader(name01).read_txt_to_str()
 replacement = replacement.split('\n')
-temp_int: list[int] = []
 new_molecule: list[str] = []
 unique_molecule: list[str] = []
 
-medicine_molecule: str = Reader(name02).read_txt_to_str()
-input_molecule: str = 'e'
+input_molecule: str = Reader(name02).read_txt_to_str()
 
-for line in replacement:
-    line = line.split(" ")
-    temp_int = [match.start() for match in finditer(line[0], input_molecule)]
-    for match in temp_int:
-        new_molecule.append(input_molecule[:match] + line[2] + input_molecule[match + len(line[0]):])
+q = Queue()
+done = Event()
+lock = Lock()
 
-for molecule in new_molecule:
-    if molecule not in unique_molecule:
-        unique_molecule.append(molecule)
+def replace(_replacement: str, _input_molecule: str):
+    for line in _replacement:
+        line = line.split(" ")
+        temp_int = [match.start() for match in finditer(line[0], _input_molecule)]
+        for match in temp_int:
+            new_molecule.append(_input_molecule[:match] + line[2] + _input_molecule[match + len(line[0]):])
 
-#TODO: alle replacemets in verschiedenen Reihnfolgen durchgehen + beachten das Buchstaben an verschiedenen Stellen verändert werden können (Threads?)
-print(unique_molecule)
-print(len(unique_molecule))
+
+num_threads = 8
+threads = [Thread(target=worker, daemon=True) for _ in range(num_threads)]
+for t in threads:
+    t.start()
+q.put((input_molecule, 0))
+done.wait()
